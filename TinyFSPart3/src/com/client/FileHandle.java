3444f377ecd34843f2a70e445a4eb5420a8e9ec0
Jessica Shin <shinjs@usc.edu>
2015-11-10 23:31:51 -0800

    FileHandle type String

--- TinyFSPart3/src/com/client/FileHandle.java
+++ TinyFSPart3/src/com/client/FileHandle.java
@@ -5,50 +5,25 @@ import java.util.Arrays;
 
 public class FileHandle {
 	final static String filePath = "csci485/";
-	public static long counter;
-	long handle;
+	String directory;
+	String filename;
+	String handle;
 	
-	
-	public FileHandle() {
-		File dir = new File(filePath);
-		File[] fs = dir.listFiles();
-		
-		if(fs.length == 0){
-			counter = 0;
-		} else {
-			int num = numFiles(fs, 0);
-			long[] allHandles = new long[num];
-			
-			findFiles(fs, allHandles);
-			Arrays.sort(allHandles);
-			counter = allHandles[num - 1];
-		}
-		handle = counter++;
+	public FileHandle(String tgtdir, String fn) {
+		directory = tgtdir;
+		filename = fn;
+		handle = directory + "/" + filename;
 	}
 	
-	public static int numFiles(File[] files, int num) {
-		for (File file : files) {
-			if (file.isDirectory()) {
-				numFiles(file.listFiles(), num);
-			} else { num++; }
-		}
-		return num;
-	}
-	
-	public static void findFiles(File[] start, long[] end) {
-		for (File file : start) {
-			int i = 0;
-			if (file.isDirectory()) {
-				findFiles(file.listFiles(), end);
-			} else {
-				end[i] = Long.valueOf(file.getName());
-				i++;
-			}
-		}
-	}
-	
-	public long getHandle() {
+	public String get() {
 		return handle;
 	}
 	
+	public String getDirectory() {
+		return directory;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
 }
