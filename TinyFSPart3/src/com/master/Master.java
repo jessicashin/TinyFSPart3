eb1a71e3e5bb26cb7de474f9ae4cb1bf1f61d669
unknown <duvoisin@db-lab10.vlab.usc.edu>
2015-11-10 20:44:37 -0800

    Initial Master.java commit.

---
+++ TinyFSPart3/src/com/master/Master.java
@@ -0,0 +1,14 @@
+package com.master;
+
+public class Master
+{
+	public static void ReadAndProcessRequests()
+	{
+		
+	}
+
+	public static void main(String args[])
+	{
+		ReadAndProcessRequests();
+	}
+}
