0d40f2d674aa7bfe238c20408f1d4f55ce181496
Jessica Shin <shinjs@usc.edu>
2015-11-17 22:09:34 -0800

    RID holds chunk handle and slot

--- TinyFSPart3/src/com/client/RID.java
+++ TinyFSPart3/src/com/client/RID.java
@@ -2,21 +2,14 @@ package com.client;
 
 public class RID {
 	String chunk;
-	int offset;
-	int length;
 	int slot;
 	
 	public RID() {
 		chunk = null;
-		offset = -1;
-		length = -1;
 		slot = -1;
 	}
-	
-	public RID(String chunkHandle, int chunkOffset, int byteLength, int slotNumber) {
+	public RID(String chunkHandle, int slotNumber) {
 		chunk = chunkHandle;
-		offset = chunkOffset;
-		length = byteLength;
 		slot = slotNumber;
 	}
 }
